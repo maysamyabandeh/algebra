53270c6f6a78a6bf6759f4f16dd7f353d27392b9
myabandeh <myabandeh@twitter.com>
2014-04-21 12:55:16 -0700

    add forgotten MergeVectorsReducer

---
+++ src/main/java/com/myabandeh/algebra/MergeVectorsReducer.java
@@ -0,0 +1,22 @@
+package com.myabandeh.algebra;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.mahout.math.SequentialAccessSparseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
+
+public class MergeVectorsReducer
+    extends
+    Reducer<WritableComparable<?>, VectorWritable, WritableComparable<?>, VectorWritable> {
+  @Override
+  public void reduce(WritableComparable<?> key,
+      Iterable<VectorWritable> vectors, Context context) throws IOException,
+      InterruptedException {
+    Vector merged = VectorWritable.merge(vectors.iterator()).get();
+    context.write(key, new VectorWritable(new SequentialAccessSparseVector(
+        merged)));
+  }
+}
