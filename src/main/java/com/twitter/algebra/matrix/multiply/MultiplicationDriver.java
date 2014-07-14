5e24dee28c65ec587c204c556d23bc3a63a8d5d8
myabandeh <myabandeh@twitter.com>
2014-07-13 19:06:59 -0700

    add driver for multiplication benchmark

---
+++ src/main/java/com/twitter/algebra/matrix/multiply/MultiplicationDriver.java
@@ -0,0 +1,96 @@
+package com.twitter.algebra.matrix.multiply;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.mahout.common.AbstractJob;
+import org.apache.mahout.common.commandline.DefaultOptionCreator;
+import org.apache.mahout.math.Matrix;
+import org.apache.mahout.math.hadoop.DistributedRowMatrix;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.twitter.algebra.AlgebraCommon;
+import com.twitter.algebra.TransposeJob;
+import com.twitter.algebra.matrix.format.Sequence2MatrixFormatJob;
+import com.twitter.algebra.matrix.multiply.AtB_DMJ;
+
+/**
+ * Benchmark multiplication algorithms
+ * 
+ * @author myabandeh
+ * 
+ */
+public class MultiplicationDriver extends AbstractJob {
+  private static final Logger log = LoggerFactory.getLogger(MultiplicationDriver.class);
+
+  private static final String BPATH = "B";
+  private static final String ROWSOPTION = "N";
+  private static final String COLSOPTION = "D";
+  private static final String PRINCIPALSOPTION = "d";
+  private static final String PARTITIONSOPTION = "kp";
+
+  @Override
+  public int run(String[] args) throws Exception {
+    addInputOption();
+    addOutputOption();
+    addOption(DefaultOptionCreator.methodOption().create());
+    addOption(BPATH, "bPath", "path to matrix B");
+    addOption(ROWSOPTION, "rows", "Number of rows");
+    addOption(COLSOPTION, "cols", "Number of cols");
+    addOption(PRINCIPALSOPTION, "pcs", "Number of principal components");
+    addOption(PARTITIONSOPTION, "parts", "Number of partitions in principal components");
+    if (parseArguments(args) == null) {
+      return -1;
+    }
+    Path input = getInputPath();
+    Path output = getOutputPath();
+    final Path bPath = new Path(getOption(BPATH));
+    final int nRows = Integer.parseInt(getOption(ROWSOPTION));
+    final int nCols = Integer.parseInt(getOption(COLSOPTION));
+    final int nPCs = Integer.parseInt(getOption(PRINCIPALSOPTION));
+    final int nColPartitions = Integer.parseInt(getOption(PARTITIONSOPTION));
+
+
+    Configuration conf = getConf();
+    if (conf == null) {
+      throw new IOException("No Hadoop configuration present");
+    }
+    
+    run(conf, input, bPath, output, nRows, nCols, nPCs, nColPartitions);
+    return 0;
+  }
+
+  private void run(Configuration conf, Path atPath, Path bPath, Path output, int nRows,
+      int nCols, int k, int nParts) throws Exception {
+    log.info("reading At");
+    DistributedRowMatrix distAt =
+        new DistributedRowMatrix(atPath, getTempPath(), nRows, nCols);
+    distAt.setConf(conf);
+
+    log.info("reading B");
+    DistributedRowMatrix distB =
+        new DistributedRowMatrix(bPath, getTempPath(), nRows, k);
+    distB.setConf(conf);
+
+    log.info("Partitioning At");
+    distAt = PartitionerJob.run(conf, distAt, nParts, atPath.getName() + "-partitioned" + nParts);
+    log.info("Partitioning B");
+    distB = PartitionerJob.run(conf, distB, nParts, bPath.getName() + "-partitioned" + nParts);
+    
+    log.info("Computing At x B");
+    DistributedRowMatrix distXt = 
+        AtBOuterStaticMapsideJoinJob.run(conf, distAt, distB, atPath.getName() + "x" + bPath.getName() + "-SMJ");
+  }
+
+  /**
+   * @param args
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new Configuration(), new MultiplicationDriver(), args);
+  }
+
+}
---
+++ src/main/java/com/twitter/algebra/matrix/multiply/PartitionerJob.java
@@ -0,0 +1,139 @@
+package com.twitter.algebra.matrix.multiply;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.mahout.common.AbstractJob;
+import org.apache.mahout.math.SequentialAccessSparseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
+import org.apache.mahout.math.hadoop.DistributedRowMatrix;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.twitter.algebra.MergeVectorsReducer;
+import com.twitter.algebra.matrix.format.MatrixOutputFormat;
+import com.twitter.algebra.matrix.format.RowPartitioner;
+
+/**
+ * Partition a row matrix into fixed number of partitions to be used by map-side
+ * join
+ * 
+ * @author myabandeh
+ * 
+ */
+public class PartitionerJob extends AbstractJob {
+  private static final Logger log = LoggerFactory
+      .getLogger(PartitionerJob.class);
+
+  @Override
+  public int run(String[] strings) throws Exception {
+    addInputOption();
+    addOption("numRows", "nr", "Number of rows of the input matrix");
+    addOption("numCols", "nc", "Number of columns of the input matrix");
+    addOption("numParts", "np", "Number of partitions of the input matrix");
+    Map<String, List<String>> parsedArgs = parseArguments(strings);
+    if (parsedArgs == null) {
+      return -1;
+    }
+
+    int numRows = Integer.parseInt(getOption("numRows"));
+    int numCols = Integer.parseInt(getOption("numCols"));
+    int numParts = Integer.parseInt(getOption("numParts"));
+
+    DistributedRowMatrix matrix =
+        new DistributedRowMatrix(getInputPath(), getTempPath(), numRows,
+            numCols);
+    matrix.setConf(new Configuration(getConf()));
+    PartitionerJob.run(getConf(), matrix, numParts, "partitioned-" + getInputPath());
+    return 0;
+  }
+
+  public static DistributedRowMatrix run(Configuration conf,
+      DistributedRowMatrix A, int partitions, String label) throws IOException,
+      InterruptedException, ClassNotFoundException {
+    log.info("running " + PartitionerJob.class.getName());
+    Path outPath = new Path(A.getOutputTempPath(), label);
+    FileSystem fs = FileSystem.get(outPath.toUri(), conf);
+    PartitionerJob job = new PartitionerJob();
+    if (!fs.exists(outPath)) {
+      job.run(conf, A.getRowPath(), outPath, A.numRows(), partitions);
+    } else {
+      log.warn("----------- Skip already exists: " + outPath);
+    }
+    DistributedRowMatrix distRes =
+        new DistributedRowMatrix(outPath, A.getOutputTempPath(), A.numRows(),
+            A.numCols());
+    distRes.setConf(conf);
+    return distRes;
+  }
+
+  public void run(Configuration conf, Path matrixInputPath,
+      Path matrixOutputPath, int aRows, int partitions) throws IOException,
+      InterruptedException, ClassNotFoundException {
+    @SuppressWarnings("deprecation")
+    Job job = new Job(conf);
+    job.setJarByClass(PartitionerJob.class);
+    job.setJobName(PartitionerJob.class.getSimpleName() + "-"
+        + matrixOutputPath.getName());
+    FileSystem fs = FileSystem.get(matrixInputPath.toUri(), conf);
+    matrixInputPath = fs.makeQualified(matrixInputPath);
+    matrixOutputPath = fs.makeQualified(matrixOutputPath);
+
+    FileInputFormat.addInputPath(job, matrixInputPath);
+    job.setInputFormatClass(SequenceFileInputFormat.class);
+    FileOutputFormat.setOutputPath(job, matrixOutputPath);
+
+    job.setNumReduceTasks(partitions);
+
+    job.setOutputFormatClass(SequenceFileOutputFormat.class);
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(VectorWritable.class);
+
+    job.setMapperClass(IdMapper.class);
+    job.setReducerClass(IdReducer.class);
+
+    RowPartitioner.setPartitioner(job, RowPartitioner.IntRowPartitioner.class,
+        aRows);
+
+    job.submit();
+    boolean res = job.waitForCompletion(true);
+    if (!res)
+      throw new IOException("Job failed!");
+  }
+
+  public static class IdMapper extends
+      Mapper<IntWritable, VectorWritable, IntWritable, VectorWritable> {
+    @Override
+    public void map(IntWritable key, VectorWritable value, Context context)
+        throws IOException, InterruptedException {
+      context.write(key, value);
+    }
+  }
+
+  public static class IdReducer
+      extends
+      Reducer<WritableComparable<?>, VectorWritable, WritableComparable<?>, VectorWritable> {
+    @Override
+    public void reduce(WritableComparable<?> key,
+        Iterable<VectorWritable> vectors, Context context) throws IOException,
+        InterruptedException {
+      context.write(key, vectors.iterator().next());
+    }
+  }
+
+}
